29123d4f98a6b0babd63c20ba940e6694117fdc7
Artzy77 <117123921+Artzy77@users.noreply.github.com>
2023-03-07 17:13:47 -0800

    commit main
    upload local repo

---
+++ data.js
@@ -0,0 +1,56 @@
+export const tweetsData = [   
+    {
+        handle: `@Bot267 💎`,
+        profilePic: `images/bot.jpg`,
+        likes: 27,
+        retweets: 10,
+        tweetText: `Like, Comment and Retweet if you believe in BTC 💰💰💰  
+            Trusted Seller. For buy inquires, dm me`,
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+    },    
+    {
+        handle: `@Elon ☑️`,
+        profilePic: `images/elon.jpg`,
+        likes: 6500,
+        retweets: 234,
+        tweetText: `I have unblocked everyone I blocked, apart from scammers. I recommend others do the same.
+
+        Negative feedback is a good thing.`,
+        replies: [
+                  {
+                handle: `@RobertDowneyJr ☑️`,
+                profilePic: `images/downey.jpg`,
+                tweetText: `Negative feedback presented in a constructive manner can lead to positive change. 💫`,
+            },
+                  {
+                handle: `@BillGates ☑️`,
+                profilePic: `images/gates.jpg`,
+                tweetText: `Someone else's success doesn't diminish your own. In fact, celebrating their achievements can inspire and motivate you to reach your own goals.`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
+    },
+        {
+        handle: `@BeginnerCoder119`,
+        profilePic: `images/cat.jpg`,
+        likes: 10,
+        retweets: 3,
+        tweetText: `I want to build a website. What do I learn first?`,
+        replies: [
+            {
+                handle: `@StackOverflowHelper`,
+                profilePic: `images/overflow.png`,
+                tweetText: `Learn HTML, CSS and JavaScript`,
+            },
+
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
+    },     
+]
